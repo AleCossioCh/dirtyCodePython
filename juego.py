5fecf0747107be0a04820f380e640baa752fb041
Pintex2 <Andreumark@hotmail.com>
2020-09-23 23:11:37 -0400

    refactored code

--- juego.py
+++ juego.py
@@ -1,70 +1,90 @@
 import random
-
+import os
 words = 'sesamo apio perejil aceite arandano gelatina jengibre higo zapallo nispero ciruelo huevo pan yogurt perro almendra mango durazno granada pera repollo espinaca yuca guayaba melon ajo limon fresa mora brocoli lechuga palta cafe avena cocoa sal azucar vainilla mantequilla carne queso leche chocolate fideo arroz rabano chirimoya mandarina pomelo naranja platano coco sandia cebolla tomate pimenton zanahoria kiwi papaya uva manzana locoto papa frutilla'.split()
-print('ADIVINA LA PALABRA')
-incorrectas = ''
-correctas = ''
+letrasIncorrectas = ''
+letrasCorrectas = ''
 indice = random.randint(0, len(words) - 1)
-palabra = words[indice]
-terminado = False
-
-while True:
-    print("#####################################################################################################")
-    print('Letras incorrectas:', end=' ')
-    for letra in incorrectas:
-        print(letra, end=' ')
-    print()
-
-    espacios = '_' * len(palabra)
-    for i in range(len(palabra)):
-        if palabra[i] in correctas:
-
-            espacios = espacios[:i] + \
-                palabra[i] + espacios[i+1:]
-
-    for letra in espacios:
-        print(letra, end=' ')
-    print()
-
+palabraSecreta = words[indice]
+juegoTerminado = False
+def mensajesIntentos():
     while True:
         print('Adivina una letra.')
         intento = input()
         intento = intento.lower()
         if len(intento) != 1:
             print('Por favor, introduce una letra.')
-        elif intento in correctas or intento in incorrectas:
+        elif intento in letrasCorrectas or intento in letrasIncorrectas:
             print('Ya has probado esa letra. Elige otra.')
         elif intento not in 'abcdefghijklmnñopqrstuvwxyz':
             print('Por favor ingresa una LETRA.')
         else:
             break
+    return intento
 
-    if intento in palabra:
-        correctas = correctas + intento
+def mostrarLetras(palabras):
+    for letra in palabras:
+        print(letra, end=' ')
+    print()
+def sinIntentos():
+    global letrasIncorrectas 
+    letrasIncorrectas = letrasIncorrectas + intento
+    if len(letrasIncorrectas) == 6:
+        print('¡Te has quedado sin intentos!\nDespués de ' + str(len(letrasIncorrectas)) +
+              ' intentos fallidos y ' + str(len(letrasCorrectas)) + ' aciertos, la palabra era "' + palabraSecreta + '"')
+        global juegoTerminado
+        juegoTerminado= True
+
+def reiniciar():
+    global letrasIncorrectas
+    letrasIncorrectas = ''
+    global letrasCorrectas
+    letrasCorrectas = ''
+    global indice
+    indice = random.randint(0, len(words) - 1)
+    global palabraSecreta
+    palabraSecreta = words[indice]
+    global juegoTerminado
+    juegoTerminado= False
+
+
+while True:
+    print('ADIVINA LA PALABRA')
+    print("#####################################################################################################")
+    print('Letras incorrectas:', end=' ')
+    mostrarLetras(letrasIncorrectas)
+
+    espacios = '_' * len(palabraSecreta)
+    for i in range(len(palabraSecreta)):
+        if palabraSecreta[i] in letrasCorrectas:
+
+            espacios = espacios[:i] + \
+                palabraSecreta[i] + espacios[i+1:]
+
+    mostrarLetras(espacios)
+
+    intento = mensajesIntentos()
+
+    if intento in palabraSecreta:
+        letrasCorrectas = letrasCorrectas + intento
         todas = True
-        for i in range(len(palabra)):
-            if palabra[i] not in correctas:
+        for i in range(len(palabraSecreta)):
+            if palabraSecreta[i] not in letrasCorrectas:
                 todas = False
-                break
-
+                return True
+            else:
+                return False
+       
         if todas:
             print('¡Sí! ¡La palabra secreta es "' +
-                  palabra + '"! ¡Has ganado!')
-            terminado = True
+                  palabraSecreta + '"! ¡Has ganado!')
+            juegoTerminado = True
     else:
-        incorrectas = incorrectas + intento
-        if len(incorrectas) == 6:
-            print('¡Te has quedado sin intentos!\nDespués de ' + str(len(incorrectas)) +
-                  ' intentos fallidos y ' + str(len(correctas)) + ' aciertos, la palabra era "' + palabra + '"')
-            terminado = True
+       sinIntentos()
 
-    if terminado:
+    if juegoTerminado:
         print('¿Quieres jugar de nuevo? (sí o no)')
         if input().lower().startswith('s'):
-            incorrectas = ''
-            correctas = ''
-            terminado = False
-            indice = random.randint(0, len(words) - 1)
-            palabra = words[indice]
+          reiniciar()
+          os.system("cls")
         else:
             break
