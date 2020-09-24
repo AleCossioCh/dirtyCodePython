8f79d8cbe41c56dd4c0a555c22f48bbf73247a71
Andre Pinto <42154799+Pintex2@users.noreply.github.com>
2020-09-24 00:18:30 -0400

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,11 +1,14 @@
 import random
 import os
+# se agrego la libreria os system para poder limpiar la consola una vez reiniciado
 words = 'sesamo apio perejil aceite arandano gelatina jengibre higo zapallo nispero ciruelo huevo pan yogurt perro almendra mango durazno granada pera repollo espinaca yuca guayaba melon ajo limon fresa mora brocoli lechuga palta cafe avena cocoa sal azucar vainilla mantequilla carne queso leche chocolate fideo arroz rabano chirimoya mandarina pomelo naranja platano coco sandia cebolla tomate pimenton zanahoria kiwi papaya uva manzana locoto papa frutilla'.split()
 letrasIncorrectas = ''
 letrasCorrectas = ''
 indice = random.randint(0, len(words) - 1)
 palabraSecreta = words[indice]
 juegoTerminado = False
+#se modifico el nombre de las variables a una mas descriptiva
+
 def mensajesIntentos():
     while True:
         print('Adivina una letra.')
@@ -20,11 +23,13 @@ def mensajesIntentos():
         else:
             break
     return intento
+#se creo nuevas funciones para tener un codigo mas legible
 
 def mostrarLetras(palabras):
     for letra in palabras:
         print(letra, end=' ')
     print()
+ #implementacion de camelCase   
 def sinIntentos():
     global letrasIncorrectas 
     letrasIncorrectas = letrasIncorrectas + intento
@@ -34,7 +39,7 @@ def sinIntentos():
         global juegoTerminado
         juegoTerminado= True
 
-def reiniciar():
+def reiniciarJuego():
     global letrasIncorrectas
     letrasIncorrectas = ''
     global letrasCorrectas
@@ -46,24 +51,18 @@ def reiniciar():
     global juegoTerminado
     juegoTerminado= False
 
-
 while True:
     print('ADIVINA LA PALABRA')
     print("#####################################################################################################")
     print('Letras incorrectas:', end=' ')
     mostrarLetras(letrasIncorrectas)
-
     espacios = '_' * len(palabraSecreta)
     for i in range(len(palabraSecreta)):
         if palabraSecreta[i] in letrasCorrectas:
-
             espacios = espacios[:i] + \
                 palabraSecreta[i] + espacios[i+1:]
-
     mostrarLetras(espacios)
-
     intento = mensajesIntentos()
-
     if intento in palabraSecreta:
         letrasCorrectas = letrasCorrectas + intento
         todas = True
@@ -76,12 +75,15 @@ while True:
                   palabraSecreta + '"! ¡Has ganado!')
             juegoTerminado = True
     else:
+        
        sinIntentos()
-
+    
     if juegoTerminado:
         print('¿Quieres jugar de nuevo? (sí o no)')
         if input().lower().startswith('s'):
-          reiniciar()
+          reiniciarJuego()
           os.system("cls")
+          #eliminacion de codigo duplicado
         else:
             break
+#se arreglo identacion
